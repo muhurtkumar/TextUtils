810bb4b3624f750c50129053fbebe1369ded816c
muhurtkumar <muhurtkumar@gmail.com>
2024-11-08 00:48:48 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">{props.aboutText}</Link>
+          <a className="nav-link" href="/about">{props.aboutText}</a>
         </li>
       </ul>
       
@@ -35,6 +35,5 @@ Navbar.propTypes = {title: PropTypes.string.isRequired, // ab title me in app.js
 }
 
 Navbar.defaultProps = {
-  title: 'Set title here' ,
-  aboutText: 'About Text here'
+  title: 'Set title here'
 }
