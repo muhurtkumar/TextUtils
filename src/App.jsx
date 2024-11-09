7c9729e4fa10801276d82e4c17abaca4b12296d1
muhurtkumar <muhurtkumar@gmail.com>
2024-11-09 23:15:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const showAlert = (message, type)=>{
         <Navbar title="Textutils"  mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+            <TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove extra spaces" mode={mode} />
         </div> 
     </>
   );
